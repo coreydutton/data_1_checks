b73322c97c7f7bea22f179828673f30f4caa7b6f
coreydutton <105877721+coreydutton@users.noreply.github.com>
2022-05-25 18:05:52 -0400

    Add files via upload

---
+++ Knowledge Check 1.py
@@ -0,0 +1,15 @@
+
+#hello world print statement
+print('hello world!')
+
+#list and print
+tv_shows = ['Criminal Minds', 'Law and Order', 'CSI']
+print(tv_shows[2])
+
+#dictionary with two keys and two values and print
+tv_shows = {'Criminal Minds': 20, 'Law and Order': 30, 'CSI': 15}
+print((tv_shows)['CSI'])
+
+#tuple with four values and print
+my_tuple = ('Criminal Minds', 'Law and Order', 'CSI')
+print(my_tuple[0])
